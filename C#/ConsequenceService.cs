4fdda8154494b7f9550ae0c826c1254313b88ef9
Joseph Aquino <110830257+ImJkino@users.noreply.github.com>
2023-08-03 08:30:17 -0700

    Update ConsequenceService.cs

--- C#/ConsequenceService.cs
+++ C#/ConsequenceService.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Services
 {
     public class ConsequenceService : IConsequenceService 
