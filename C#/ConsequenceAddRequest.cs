002a9c92abe47717703d85c92aaceb91d6bf5769
Joseph Aquino <110830257+ImJkino@users.noreply.github.com>
2023-08-10 08:43:39 -0700

    Update ConsequenceAddRequest.cs

--- C#/ConsequenceAddRequest.cs
+++ C#/ConsequenceAddRequest.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Models.Requests.Consequences
 {
     public class ConsequenceAddRequest
