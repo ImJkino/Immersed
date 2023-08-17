d0f75450ff34c7733c27338eb362d4accd6de790
Joseph Aquino <110830257+ImJkino@users.noreply.github.com>
2023-08-17 08:06:48 -0700

    Update ConsequenceUpdateRequest.cs

--- C#/ConsequenceUpdateRequest.cs
+++ C#/ConsequenceUpdateRequest.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Consequences
+public class ConsequenceUpdateRequest : ConsequenceAddRequest
 {
-    public class ConsequenceUpdateRequest : ConsequenceAddRequest
-    {
-        [Required, Range(1, Int32.MaxValue)]
-        public int Id { get; set; }
-        public bool IsActive { get; set; }
-        public bool isDeleted { get; set; }
-    }
+    [Required, Range(1, Int32.MaxValue)]
+    public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public bool isDeleted { get; set; }
 }
