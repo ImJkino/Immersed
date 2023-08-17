41151265ac82e097cc7307a6ff7e4f68eed9bbaf
Joseph Aquino <110830257+ImJkino@users.noreply.github.com>
2023-08-17 08:08:45 -0700

    Update ConsequenceAddRequest.cs

--- C#/ConsequenceAddRequest.cs
+++ C#/ConsequenceAddRequest.cs
@@ -1,18 +1,13 @@
-namespace Models.Requests.Consequences
+public class ConsequenceAddRequest
 {
-    public class ConsequenceAddRequest
-    {
-        [Required, MinLength(2), MaxLength(100)]
-        public string Name { get; set; }
-        [Required, MinLength(2), MaxLength(500)]
-        public string Description { get; set; }
-        [Required, Range(1, Int32.MaxValue)]
-        public int ConsequenceTypeId { get; set; }
-        [Required, Range(1, Int32.MaxValue)]
-        public int ActorId { get; set; }
-        [AllowNull]
-        public int? ZoneId { get; set; }
-    }
+    [Required, MinLength(2), MaxLength(100)]
+    public string Name { get; set; }
+    [Required, MinLength(2), MaxLength(500)]
+    public string Description { get; set; }
+    [Required, Range(1, Int32.MaxValue)]
+    public int ConsequenceTypeId { get; set; }
+    [Required, Range(1, Int32.MaxValue)]
+    public int ActorId { get; set; }
+    [AllowNull]
+    public int? ZoneId { get; set; }
 }
-
-
